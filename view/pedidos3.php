713e935ae5f4cb949fc471712fc45fa952090a9f
MaiconSpielmann <spielmann.maicon@gmail.com>
2015-11-07 19:14:46 -0200

    Teste 3

---
+++ view/pedidos3.php
@@ -0,0 +1,75 @@
+<?php 
+  require_once("cabecalho.php"); 
+  require_once("menu.php");
+  require_once($_SERVER['DOCUMENT_ROOT']."/controller/conecta.php");
+  require_once($_SERVER['DOCUMENT_ROOT']."/controller/funcoes_pedidos.php");
+  require_once($_SERVER['DOCUMENT_ROOT']."/controller/funcoes_login.php");
+?>
+<div id="page-wrapper">
+    <div class="row">
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-18">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                   Pedidos
+                </div>
+                <!-- /.panel-heading -->
+                <div class="panel-body">
+                	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.9/css/jquery.dataTables.min.css">
+					<table id="example" class="display" width="100%"></table>
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    </div>
+    <!-- /.panel-body -->
+</div>
+<!-- /.panel -->
+</div>
+    <!-- /.col-lg-6 -->
+</div>
+<!-- /.row -->
+</div>
+
+<script type="text/javascript">
+var dataSet = [
+</script>
+
+<?php
+  $usuario  = buscaIdUsuario($conexao, usuarioLogado());
+  $pedidos = listaPedidos($conexao,$usuario["idempresa"]);
+ 
+  foreach ($pedidos as $pedido) {
+  	echo '["'.$pedido["idpedido"].'", "'.$pedido["nomecliente"].'", "'.$pedido["nomeprato"].'", "'.$pedido["quantidade"].'", "'.$pedido["observacao"].'", "'.$pedido["cidade"].'", "'.$pedido["descrpagamento"].'", "'.$pedido["status"].'"],';
+  }
+?>
+
+<script type="text/javascript">
+];
+
+$(document).ready(function() {
+    $('#example').DataTable( {
+        data: dataSet,
+        columns: [
+            { title: "Idpedido" },
+            { title: "Nome Cliente" },
+            { title: "Cardápio" },
+            { title: "Quantidade" },
+            { title: "Observação" }
+            { title: "Endereço" }
+            { title: "Forma de Pagamento" }
+            { title: "Status" }
+        ]
+    } );
+} );
+
+</script>
+
+<!-- jQuery -->
+<script src="https://cdn.datatables.net/1.10.9/js/jquery.dataTables.min.js"></script>
